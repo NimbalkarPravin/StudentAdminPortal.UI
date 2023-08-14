cafd44dcb44e2e3fafb4f57fa20bc3190a195d8a
NimbalkarPravin <nimbalkarpravin2707@gmail.com>
2023-08-14 20:38:15 +0530

    End Of Section 3

---
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseApiUrl = 'https://localhost:7199';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getStudent(): Observable<student[]> {
+    return this.httpClient.get<student[]>(this.baseApiUrl + '/students');
+  }
+
+}
